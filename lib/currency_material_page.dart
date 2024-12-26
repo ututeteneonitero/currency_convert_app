e6144724c6535aeebf57a19c942801ed6de09a37
unknown <mco908472@gmail.com>
2024-12-26 15:29:45 +0700

    use padding

--- lib/currency_material_page.dart
+++ lib/currency_material_page.dart
@@ -21,27 +21,29 @@ class CurrencyMaterialPage extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-              TextField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                decoration: InputDecoration(
-                  hintText: "Enter amount in USD",
-                  hintStyle: TextStyle(color: Colors.white70),
-                  prefixIcon: Icon(Icons.monetization_on_outlined,
-                      color: Colors.white70),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: TextField(
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white54),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  decoration: InputDecoration(
+                    hintText: "Enter amount in USD",
+                    hintStyle: TextStyle(color: Colors.white70),
+                    prefixIcon: Icon(Icons.monetization_on_outlined,
+                        color: Colors.white70),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white54),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
                   ),
-                  
-                ),
-                keyboardType: const TextInputType.numberWithOptions(
-                  decimal: true,
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                  ),
                 ),
               ),
             ],
